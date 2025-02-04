93eda8b2003e86d15dd0bce1cc0afaee3034f71c
Kam Lasater <kam@manufactured.com>
2025-02-03 23:13:23 -0500

    zod to the rescue

--- src/step1/index.js
+++ src/step1/index.js
@@ -1,42 +1,30 @@
-// From OpenAI docs:
-// https://platform.openai.com/docs/api-reference/chat/create
+import { z } from 'zod'
+import { zodResponseFormat } from 'openai/helpers/zod';
 import OpenAI from "openai";
 
 const openai = new OpenAI();
 
 const prompt = "What is the average wing speed of a swallow?"
 
+const doneResponseSchema = z.object({
+  done: z.boolean(),
+});
+
 async function main() {
   const completion = await openai.chat.completions.create({
     messages: [{ role: "developer", content: prompt }],
-    model: "gpt-4o",
+    model: "gpt-4o-mini",
   });
 
   console.log(completion.choices[0].message);
 
   const check = await openai.chat.completions.create({
-    model: "gpt-4o",
+    model: "gpt-4o-mini",
     messages: [{
       role: "developer",
       content: `Given the following question, determine if the answer is sufficent.\n\nQuestion: ${prompt}\n\nAnswer: ${completion.choices[0].message.content}`,
     }],
-    response_format: {
-      "type": "json_schema",
-      "json_schema": {
-        name: "conditionalCheck",
-        schema: {
-          "$schema": "https://json-schema.org/draft/2020-12/schema",
-          "type": "object",
-          "properties": {
-            "done": {
-              "type": "boolean"
-            }
-          },
-          "required": ["done"],
-          "additionalProperties": false
-        }
-      }
-    }
+    response_format: zodResponseFormat(doneResponseSchema, "doneResponseSchema")
   })
   console.log(check.choices[0].message)
 }
