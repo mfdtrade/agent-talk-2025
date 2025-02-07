4bfdf6392b9f90f7b9fd4e79ae01d8cadf4124e6
Kam Lasater <kam@manufactured.com>
2025-02-07 09:15:24 -0500

    checkpoint

---
+++ src/_steps/step4-planning.js
@@ -0,0 +1,38 @@
+import { z } from 'zod'
+import { zodResponseFormat } from 'openai/helpers/zod';
+import tools from '../tools/index.js';
+import { completeWithTools, openai } from '../utils/ai.js';
+
+const goal = process.argv[2] || "I want to learn about building agents without a framework."
+
+
+const prompt = `
+You are a helpful assistant working for a busy executive.
+Your tone is friendly but direct, they prefer short clear and direct writing.
+You try to accomplish the specific task you are given.
+You can use any of the tools available to you.
+Before you do any work you always make a plan using your Todo list.
+You can mark todos off on your todo list after they are complete.
+When you have completed your task you always checkGoalDone.
+
+You summarize the actions you took checking the done list then report back what you found to your user.
+`
+
+async function main() {
+  const completion = await completeWithTools({
+    messages: [
+      { role: "developer", content: prompt },
+      { role: "user", content: goal}
+    ],
+    model: "gpt-4o-mini",
+    tool_choice: "auto",
+    tools: tools.configsArray,
+    store: false
+  });
+
+  const answer = completion.choices[0].message.content
+  console.log("\n\n"+"#".repeat(40));
+  console.log(`Answer: ${answer}`);
+}
+
+main();
---
+++ src/tools/llmJudge.js
@@ -0,0 +1,30 @@
+import { z } from "zod";
+import { zodFunction } from "openai/helpers/zod";
+import { completeWithTools } from '../utils/ai.js'
+
+
+// ####################################################################################
+// CREATE - Add New Todos
+export async function checkGoalDone(goal, answer) {
+  const resp = await completeWithTools({
+    model: "gpt-4o-mini",
+    messages: [{
+      role: "developer",
+      content: `You are a strict critic. Given the following question, determine if the answer a full anwser to the question.\n\nQuestion: ${goal}\n\nAnswer: ${answer}`,
+    }],
+    response_format: zodResponseFormat(z.object({done: z.boolean()}), "doneResponseSchema")
+  })
+  const check = JSON.parse(resp.choices[0].message.content)
+  console.log("\n\n"+"#".repeat(40));
+  console.log(`LLM as judge: ${ check.done ? "👍":"👎"}`)
+  return JSON.stringify(check)
+}
+
+export const checkGoalDoneToolConfig = zodFunction({
+    name: "checkGoalDone",
+    description: "Check if the answer successfully meets the requested goal.",
+    parameters: z.object({
+        goal: z.string().describe("The requested goal to be completed."),
+        answer: z.string().describe("The answer that will be provided to the requesting party to complete that goal.")
+    })
+})
